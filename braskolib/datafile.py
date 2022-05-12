d100cc0a6b19db6d290f6190c980aab0b9f8e97c
mboudet <mateo.boudet@gmail.com>
2022-05-12 18:27:16 +0200

    Test integrate

---
+++ braskolib/datafile.py
@@ -0,0 +1,86 @@
+from pathlib import Path
+import os
+import json
+
+
+class Datafile():
+
+    def __init__(self, pattern, integration_file, search_folder, askomics_client, type="csv"):
+        current_args = locals()
+        self.pattern = pattern
+        self.search_folder = search_folder
+        self.files = {}
+        self.type = type
+        self.askomics_client = askomics_client
+        self.files_to_integrate = []
+
+        with open(integration_file) as datafile:
+            self.integration_template = json.load(datafile)
+
+    def get_files(self):
+        file_dict = {}
+        for path in Path(self.search_folder).rglob(self.pattern):
+            head, tail = os.path.split(path)
+            if head not in file_dict:
+                file_dict[head] = []
+            file_dict[head].append({"file": tail, "time": os.path.getctime(path)})
+
+        self.files = file_dict
+
+    def cleanup_askomics(self):
+        datasets = self.askomics_client.dataset.list()
+        files = self.askomics_client.file.list()
+
+        names_to_delete = []
+        files_to_delete = []
+        keys_to_delete = []
+
+        for key, path in self.files.items():
+            for value in path:
+                file_name = value["file"]
+                for file in files:
+                    if file["name"] == file_name:
+                        if file["date"] < value["time"]:
+                            files_to_delete.append(file["id"])
+                            names_to_delete.append(file_name)
+                            print("Deleting remote obsolete file " + file_name)
+                        else:
+                            print("Skipping older local file " + file_name)
+                            keys_to_delete.append(key)
+
+        datasets_to_delete = [dataset["id"] for dataset in datasets if dataset["name"] in names_to_delete]
+
+        if datasets_to_delete:
+            self.askomics_client.dataset.delete(datasets_to_delete)
+        if files_to_delete:
+            self.askomics_client.file.delete(files_to_delete)
+
+        if keys_to_delete:
+            for key in keys_to_delete:
+                self.files.pop(key, None)
+
+        self.files_to_integrate = []
+        for key, path in self.files.items():
+            self.files_to_integrate.append(os.path.join(key, path))
+
+    def upload_files(self):
+        for file_path in self.files_to_integrate:
+            self.askomics_client.file.upload(file_path=file_path)
+
+    def integrate_files(self):
+        uploaded_files = set([os.path.basename(file) for file in self.files_to_integrate])
+        files = self.askomics_client.file.list()
+        file_ids = set()
+
+        for file in files:
+            if file["name"] in uploaded_files:
+                file_ids.add(file["id"])
+        for id in file_ids:
+            try:
+                if self.type == "csv":
+                    self.askomics_client.file.integrate_csv(id, columns=self.integration_template["columns"], headers=self.integration_template["headers"], force=True)
+                elif self.type == "gff":
+                    self.askomics_client.file.integrate_gff(id, entities=self.integration_template["columns"])
+            except Exception as e:
+                print(id)
+                raise e
---
+++ load.py
@@ -0,0 +1,102 @@
+import askoclics
+
+import configparser
+
+from braskolib.datafile import Datafile
+
+
+def main():
+
+    config = configparser.ConfigParser()
+    config.read("deepimpactomics.conf")
+
+    asko_client = askoclics.AskomicsInstance(url=config['askomics']['url'], api_key=config['askomics']['api_key'], proxy_username=config['askomics'].get('proxy_username'), proxy_password=config['askomics'].get('proxy_password'))
+
+    search_folder = config['main']['search_folder']
+
+    datafiles = [
+        Datafile(
+            pattern="Asko*.txt",
+            integration_file="templates/askomics/differential_expression.json",
+            search_folder=search_folder,
+            askomics_client=asko_client
+        ),
+        Datafile(
+            pattern="*.gff*",
+            integration_file="templates/askomics/differential_expression.json",
+            search_folder=search_folder,
+            askomics_client=asko_client,
+            type="gff"
+        ),
+        Datafile(
+            pattern="condition*.txt",
+            integration_file="templates/askomics/condition.json",
+            search_folder=search_folder,
+            askomics_client=asko_client
+        ),
+        Datafile(
+            pattern="context*.txt",
+            integration_file="templates/askomics/context.json",
+            search_folder=search_folder,
+            askomics_client=asko_client
+        ),
+        Datafile(
+            pattern="contrast*.txt",
+            integration_file="templates/askomics/contrast.json",
+            search_folder=search_folder,
+            askomics_client=asko_client
+        ),
+        Datafile(
+            pattern="dilution*.txt",
+            integration_file="templates/askomics/dilution.json",
+            search_folder=search_folder,
+            askomics_client=asko_client
+        ),
+        Datafile(
+            pattern="otu_dil*.txt",
+            integration_file="templates/askomics/otu_count.json",
+            search_folder=search_folder,
+            askomics_client=asko_client
+        ),
+        Datafile(
+            pattern="otu_mean*.txt",
+            integration_file="templates/askomics/otu_count_mean.json",
+            search_folder=search_folder,
+            askomics_client=asko_client
+        ),
+        Datafile(
+            pattern="OTUs.txt",
+            integration_file="templates/askomics/otus.json",
+            search_folder=search_folder,
+            askomics_client=asko_client
+        ),
+        Datafile(
+            pattern="soils*.txt",
+            integration_file="templates/askomics/soils.json",
+            search_folder=search_folder,
+            askomics_client=asko_client
+        ),
+        Datafile(
+            pattern="Taxons.txt",
+            integration_file="templates/askomics/taxon.json",
+            search_folder=search_folder,
+            askomics_client=asko_client
+        ),
+        Datafile(
+            pattern="tables1.csv",
+            integration_file="templates/askomics/physicochemical.json",
+            search_folder=search_folder,
+            askomics_client=asko_client
+        )
+    ]
+
+    # Cleanup
+    for datafile in datafiles:
+        datafile.get_files()
+        datafile.cleanup_askomics()
+        datafile.upload_files()
+        datafile.integrate_files()
+
+
+if __name__ == "__main__":
+    main()
